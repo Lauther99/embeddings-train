ec19bdeb5255e989c7edd702206c0cd3de688c36
Ubuntu <azureuser@FA100-VM.3akolfo4yyze5j22ijvzjxgtfh.cx.internal.cloudapp.net>
2025-01-29 20:29:21 +0000

    created trainer and evaluator file

---
+++ trainer/evaluator.py
@@ -0,0 +1,39 @@
+from sentence_transformers import SentenceTransformer
+from datasets import load_dataset
+
+eval_dataset = load_dataset("Lauther/embeddings-train-semantic", split="validation")
+
+
+model_name = "intfloat/e5-mistral-7b-instruct"
+
+model_name = "Alibaba-NLP/gte-Qwen2-1.5B-instruct"
+model_name = "NovaSearch/stella_en_400M_v5"
+model_name = "intfloat/multilingual-e5-large-instruct"
+model_name = "NovaSearch/stella_en_1.5B_v5"
+
+
+model_name = "Alibaba-NLP/gte-large-en-v1.5"
+model_name = "jxm/cde-small-v2"
+model_name = "all-mpnet-base-v2"
+model_name = "all-distilroberta-v1"
+
+model = SentenceTransformer(model_name, device="cuda", trust_remote_code=True)
+
+from sentence_transformers.evaluation import (
+    EmbeddingSimilarityEvaluator,
+)
+
+dev_evaluator = EmbeddingSimilarityEvaluator(
+    sentences1=eval_dataset["sentence1"],
+    sentences2=eval_dataset["sentence2"],
+    scores=eval_dataset["score"],
+    name="sts_dev",
+)
+
+results = dev_evaluator(model)
+
+print("\n\n",model_name)
+print(dev_evaluator.primary_metric)
+# => "sts_dev_pearson_cosine"
+print(results[dev_evaluator.primary_metric])
+# => 0.881019449484294
